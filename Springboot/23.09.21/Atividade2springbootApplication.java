37fa6e150f9a898099807b21e8fabf900104635f
Raniel Mendes <raniel.mendes@hotmail.com>
2021-09-23 14:16:54 -0300

    23;09;21 - Springboot

---
+++ Springboot/23.09.21/Atividade2springbootApplication.java
@@ -0,0 +1,13 @@
+package com.atividadespringboot.atividade2springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade2springbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade2springbootApplication.class, args);
+	}
+
+}
---
+++ Springboot/23.09.21/AtividadespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.atividadespringboot.atividadespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadespringbootApplication.class, args);
+	}
+
+}
